28117ef89f94855655895d68e67868a647d39cd4
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-03-13 12:30:23 -0300

    feat: new user service

--- src/backend/constants/messages/server.py
+++ src/backend/constants/messages/server.py
@@ -1,3 +1,5 @@
 ERROR_USER_REPOSITORY_CREATE_INPUT_TYPE = "O input deve ser um objeto UserModel"
 ERROR_USER_REPOSITORY_GET_INPUT_TYPE = "O input deve ser um objeto UserLogin"
 ERROR_USER_REPOSITORY_UPDATE_INPUT_TYPE = "O input deve ser um objeto UserRegister"
+
+ERROR_SERVER = "Erro no servidor"
