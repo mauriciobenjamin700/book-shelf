af8b7ee29158e851165d6fcc54cbc8bcd331222b
clistenesrodger <clistenesak47@gmail.com>
2025-03-13 20:06:38 -0300

    adicionado novas pages

---
+++ src/frontend/services/user.py
@@ -0,0 +1,8 @@
+def user_login(email: str , password: str) -> bool:
+    if(
+        email == "admin@google.com" and
+        password == "Maur1c10@"
+    ):
+        return True
+    return False
+    
