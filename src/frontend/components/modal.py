449059bddd37ce0f5fb01a004af21f2724225c4a
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-03-14 01:22:32 -0300

    feat: register are ok now

--- src/frontend/components/modal.py
+++ src/frontend/components/modal.py
@@ -12,47 +12,55 @@ class ModalTypes(Enum):
 def main(master: tk.Tk, title: str, message: str, type: ModalTypes):
     popup = tk.Toplevel(master)
     popup.geometry(get_center(master))
-    popup.configure(bg=MODALS_BACKGROUND.get("yellow"))  # Define o fundo amarelo
+    popup.configure(bg="#FFB800")  # Define o fundo amarelo
     popup.overrideredirect(True)  # Remove a barra de título
 
     # Criando um frame para a caixa de diálogo
-    frame = tk.Frame(
-        popup, 
-        bg=MODALS_BACKGROUND.get("yellow"), 
-        bd=2, 
-        border=0, 
-        relief="ridge"
-    )
-    frame.place(relx=0.5, rely=0.5, anchor="center", width=250, height=150)
+    frame = tk.Frame(popup, bg="#FFB800", bd=0)  # Removendo a borda
+    frame.place(relx=0.5, rely=0.5, anchor="center", width=400, height=200)  # Aumentando o tamanho do popup
 
     # Rótulo de texto
     label = tk.Label(
         frame,
-        text="Você tem certeza\nque deseja sair?",
-        font=("Arial", 12, "bold"),
-        bg=MODALS_BACKGROUND.get("yellow"),
+        text=message,
+        font=("Arial", 14, "bold"),  # Aumentando o tamanho da fonte
+        bg="#FFB800",
         fg="white"
     )
-    label.pack(pady=15)
+    label.pack(pady=20)
+    
+    if type == ModalTypes.ERROR:
 
-    btn_sair = tk.Button(
-        frame,
-        text="Sair",
-        font=("Arial", 10),
-        bg="white",
-        fg="black",
-        width=20,
-        command=master.quit
-    )
-    btn_sair.pack(pady=5)
+        btn_sair = tk.Button(
+            frame,
+            text="Sair",
+            font=("Arial", 12),  # Aumentando o tamanho da fonte
+            bg="white",
+            fg="black",
+            width=20,
+            command=master.quit
+        )
+        btn_sair.pack(pady=10)
 
-    btn_cancelar = tk.Button(
-        frame,
-        text="Cancelar",
-        font=("Arial", 10),
-        bg="lightgray",
-        fg="black",
-        width=20,
-        command=popup.destroy  # Corrigido para fechar o popup
-    )
-    btn_cancelar.pack(pady=5)
+        btn_cancelar = tk.Button(
+            frame,
+            text="Cancelar",
+            font=("Arial", 12),  # Aumentando o tamanho da fonte
+            bg="lightgray",
+            fg="black",
+            width=20,
+            command=popup.destroy  # Corrigido para fechar o popup
+        )
+        btn_cancelar.pack(pady=10)
+        
+    else:
+        btn_ok = tk.Button(
+            frame,
+            text="OK",
+            font=("Arial", 12),  # Aumentando o tamanho da fonte
+            bg="white",
+            fg="black",
+            width=20,
+            command=popup.destroy
+        )
+        btn_ok.pack(pady=10)
